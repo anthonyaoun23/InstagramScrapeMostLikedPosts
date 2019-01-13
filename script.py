028f261225e713e6a7446b3ba2007906624417a6
Anthony Aoun <anthony.aoun1@hotmail.com>
2019-01-13 11:04:37 -0500

    updated scrypt

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 from instaloader import Instaloader, Profile
+import os.path
 
 PROFILE = "mindoftoto"   # Insert profile name here
 
@@ -12,4 +13,13 @@ posts_sorted_by_likes = sorted(
     profile.get_posts(), key=lambda post: post.likes)
 
 # Download the post with the most likes
+current_directory = os.getcwd()
+for x in range(10):
+    final_directory = os.path.join(current_directory, str(x))
+
+
 L.download_post(posts_sorted_by_likes[len(posts_sorted_by_likes)-1], PROFILE)
+print(posts_sorted_by_likes[len(posts_sorted_by_likes)-1])
+
+if not os.path.exists(final_directory):
+    os.makedirs(final_directory)
